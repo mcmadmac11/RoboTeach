4abbc51e0ad2aa33f8fbf19eb17b98528afe2c52
Dan McManus <mcmanus.dbm@gmail.com>
2015-11-28 21:23:24 -0600

    hopefully my LAST initial commit for this project!

---
+++ Teaching/GradeBook.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Teaching
+{
+    class GradeBook   
+    {
+
+        public GradeBook()
+        {
+            grades = new List<float>();      //New object created
+
+        }
+
+        public GradeStatistics ComputeStatistics()
+        {
+            GradeStatistics stats =  new GradeStatistics();
+            float sum = 0;
+            foreach(float grade in grades)
+            {
+                stats.HighestGrade = Math.Max(grade, stats.HighestGrade);
+                stats.LowestGrade = Math.Min(grade, stats.LowestGrade);
+                sum += grade;
+            }
+            stats.AverageGrade = sum / grades.Count;
+            return stats;
+         }
+
+        public void AddGrade(float grade)
+        {
+            grades.Add(grade);
+        }
+
+        List<float> grades;
+
+     }
+      
+      
+}     
+      
---
+++ Teaching/GradeStatistics.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace Teaching
+{
+    class GradeStatistics
+    {
+        public GradeStatistics()
+        {
+            HighestGrade = 0;
+            LowestGrade = float.MaxValue;
+        }
+
+        public float HighestGrade;
+        public float LowestGrade;
+        public float AverageGrade;
+
+
+    }
+}
